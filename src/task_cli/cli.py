11514104f949ae6ea0b6f67e28ee623b8bac6d25
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-09-17 19:47:32 +0200

    feat(cli): Implémentation des gestionnaires des sous-commandes "add" et "list"

--- src/task_cli/cli.py
+++ src/task_cli/cli.py
@@ -1,6 +1,7 @@
 "Module de point d'entrée de l'application"
 
 import argparse
+import sys
 
 
 # Création un parser de ligne de commande
@@ -63,21 +64,34 @@ def create_parser() -> argparse.ArgumentParser:
 
 
 # Gestion de la commande "add"
-def handle_add(task: list[str]):
+def handle_add(tasks: list[str]) -> int | None:
     """
     Gère la commande d'ajout de tâche.
     """
 
-    # Gestion de la liste de tâche
-    print(f'Tâche ajoutée : {task}')
+    # Récupération de la liste de tâches
+    for numb, task in enumerate(tasks, start=1):
+        # Gestion des erreurs
+        task = task.strip()
+
+        if not task:
+            print("Erreur : La description est invalide", file=sys.stderr)
+            return 1
+
+        # Gestion des tâches
+        print(f"[{numb}] - {task}")
 
 
 # Gestion de la commande "list"
-def handle_list(status: str | None):
+def handle_list(status: str | None) -> str:
     """
     Gère la commande de liste des tâches.
     """
-    print(f'Tâches affichées : {status}')
+    # Gestion des erreurs
+    if status is None:
+        return "Afficher la liste de toutes les tâches."
+
+    return f'Afficher la liste des tâches avec le statut « {status} »'
 
 
 def main():
@@ -88,9 +102,11 @@ def main():
 
     if args.command == "add":
         handle_add(args.task)
+        return 0
     elif args.command == "list":
         handle_list(args.status)
+        return 0
 
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
