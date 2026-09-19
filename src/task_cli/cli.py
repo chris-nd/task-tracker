81e49cfd09a7d5b2f392a6cfedad79ba4c853561
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-09-19 18:46:39 +0200

    feat(cli): gestion de l'ajout des tâches

--- src/task_cli/cli.py
+++ src/task_cli/cli.py
@@ -1,9 +1,8 @@
 "Module de point d'entrée de l'application"
 
 import argparse
-import sys
 
-from task_cli.task import list_tasks
+from task_cli.task import create_tasks, list_tasks
 
 
 # Création un parser de ligne de commande
@@ -65,37 +64,6 @@ def create_parser() -> argparse.ArgumentParser:
     return parser
 
 
-# Gestion de la commande "add"
-def handle_add(tasks: list[str]) -> int | None:
-    """
-    Gère la commande d'ajout de tâche.
-    """
-
-    # Récupération de la liste de tâches
-    for numb, task in enumerate(tasks, start=1):
-        # Gestion des erreurs
-        task = task.strip()
-
-        if not task:
-            print("Erreur : La description est invalide", file=sys.stderr)
-            return 1
-
-        # Gestion des tâches
-        print(f"[{numb}] - {task}")
-
-
-# Gestion de la commande "list"
-def handle_list(status: str | None) -> str:
-    """
-    Gère la commande de liste des tâches.
-    """
-    # Gestion des erreurs
-    if status is None:
-        return "Afficher la liste de toutes les tâches."
-
-    return f'Afficher la liste des tâches avec le statut « {status} »'
-
-
 def main():
     "Fonction de lancement du programme."
 
@@ -103,7 +71,7 @@ def main():
     args = parser.parse_args()
 
     if args.command == "add":
-        handle_add(args.task)
+        create_tasks(args.task)
         return 0
     elif args.command == "list":
         list_tasks()
